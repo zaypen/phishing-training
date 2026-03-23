dcd441172ad76f2839ff52505a4f91c574f54cd6
zaypen <zaypen@gmail.com>
2026-03-23 15:37:48 +0800

    Add messages

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-say "You are hacked"
+echo "You are hacked"
+say "You are hacked" &
+osascript -e 'display dialog "DO NOT trust ANY script unless you inspect it" with title "You are hacked" buttons {"I wont next time"} default button "I wont next time"'

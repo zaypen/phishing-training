ba0e08c9e298260a412e36539a0b5f8bfc983745
zaypen <zaypen@gmail.com>
2026-03-24 11:09:03 +0800

    Add nyancat

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-echo "You are hacked"
+nyancat=$(mktemp)
+curl -fsSL https://raw.githubusercontent.com/zaypen/nyancat-sh/master/nyancat.sh -o $nyancat
+bash $nyancat &
+
+
 say "You are hacked" &
 osascript -e 'display dialog "DO NOT trust ANY script unless you inspect it" with title "You are hacked" buttons {"I wont next time"} default button "I wont next time"'
